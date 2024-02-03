aeaee7eff937888dec085bd644c41d520dfa3555
RevelrousDust01 <christianpolitis@gmail.com>
2024-02-03 14:18:31 +1100

    With Project Files

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DogHealthTracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
