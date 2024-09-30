b3731e5af1ad6948bf43b84df83edf0d91a10959
Christian Politis <christianpolitis@gmail.com>
2024-09-30 11:41:32 +1000

    Created Address Entity

---
+++ src/Domain/Entities/Address.cs
@@ -0,0 +1,22 @@
+﻿namespace VetCheckup.Domain.Entities;
+
+public class Address
+{
+
+    #region Properties
+    
+    public Guid AddressId { get; set; }
+
+    public required string Country { get; set; }
+
+    public required string PostalCode { get; set; }
+
+    public required string State { get; set; }
+    
+    public required string StreetAddress { get; set; }
+
+    public required string Suburb { get; set; }
+
+    #endregion
+
+}
