621c9ec85296609f73ccdfebbb05d6e479525633
Christian Politis <christianpolitis@gmail.com>
2024-09-30 11:12:36 +1000

    Created Contact Entity #13

---
+++ src/Domain/Entities/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace VetCheckup.Domain.Entities;
+
+public class Contact
+{
+    
+    #region Properties
+    
+    public int ContactId { get; set; }
+
+    public required string Email { get; set; }
+
+    public int? Mobile { get; set; } 
+    
+    #endregion
+
+}
