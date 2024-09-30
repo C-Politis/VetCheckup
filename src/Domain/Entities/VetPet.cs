00d8cf311ee27e6be3c15366aa3bdb76621a7e03
Christian Politis <christianpolitis@gmail.com>
2024-09-30 14:06:40 +1000

    Create VetPet Reference Entity
    #15

---
+++ src/Domain/Entities/VetPet.cs
@@ -0,0 +1,14 @@
+﻿namespace VetCheckup.Domain.Entities;
+
+public class VetPet
+{
+
+    #region Properties
+
+    public Guid PetId { get; set; }
+
+    public Guid VetId { get; set; }
+
+    #endregion
+
+}
