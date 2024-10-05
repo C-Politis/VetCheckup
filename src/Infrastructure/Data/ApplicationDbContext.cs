4be09669cf4b0a0483a1e0910949d42deb74ed89
Jacob Tuckwell <73861160+Necropixel15616@users.noreply.github.com>
2024-10-06 10:39:08 +1100

    Add ApplicationDbContext and Infrastructure AssemblyUtility
    #3
    #4
    #6

---
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VetCheckup.Infrastructure.Data;
+public class ApplicationDbContext : DbContext
+{
+
+    #region Methods
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(AssemblyUtility.GetAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    #endregion
+
+}
