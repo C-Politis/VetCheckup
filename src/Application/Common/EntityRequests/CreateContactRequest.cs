f3b6a59777ab1328b545e4dbdf53c5fff660f1e7
Jacob Tuckwell <73861160+Necropixel15616@users.noreply.github.com>
2024-10-13 10:53:11 +1100

    Add CreateAddressRequest and CreateContactRequest

---
+++ src/Application/Common/EntityRequests/CreateContactRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace VetCheckup.Application.Common.EntityRequests;
+
+public class CreateContactRequest
+{
+
+    #region Properties
+
+    public string? Email { get; set; }
+
+    public string? Mobile { get; set; }
+
+    #endregion
+
+}
