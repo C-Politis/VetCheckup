c9488bd60ef15ff00ca7021bbf1984191bada6f0
LeonSun1101 <151193242+leonsun1101@users.noreply.github.com>
2025-03-13 18:06:12 +1100

    Completed Organisation Entity and OrganisationType Enum

---
+++ src/Domain/Enums/OrganisationType.cs
@@ -0,0 +1,22 @@
+﻿namespace VetCheckup.Domain.Enums;
+
+public enum OrganisationType
+{
+
+    #region Enums
+
+    Clinic = 0,
+
+    Daycare = 1,
+
+    Hospital = 2,
+
+    Rescues = 3,
+
+    Shelter = 4,
+
+    Other = 5
+
+    #endregion
+
+}
