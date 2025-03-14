b02c429f1acf36ac5b5deee8027fde2112b18c09
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-15 10:43:45 +1100

    CreateOrganisationRequestValidator written

---
+++ src/Application/UseCases/Organisations/CreateOrganisation/CreateOrganisationRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using VetCheckup.Application.Common.EntityRequestValidators;
+
+namespace VetCheckup.Application.UseCases.Organisations.CreateOrganisation;
+
+public class CreateOrganisationRequestValidator : AbstractValidator<CreateOrganisationRequest>
+{
+
+    #region Constructors
+
+    public CreateOrganisationRequestValidator()
+    {
+        _ = this.RuleFor(e => e.Address).SetValidator(new CreateAddressRequestValidator());
+        _ = this.RuleFor(e => e.ContactDetails).SetValidator(new CreateContactRequestValidator());
+
+        _ = this.RuleFor(e => e.Name)
+            .MaximumLength(100)
+            .NotEmpty();
+
+        _ = this.RuleFor(e => e.OrganisationType)
+            .IsInEnum();
+    }
+
+    #endregion
+
+}
