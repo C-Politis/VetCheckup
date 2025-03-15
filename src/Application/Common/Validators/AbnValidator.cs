2854c1aa2c1e90608bbe8bf7cc5c5de7192f8aea
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-15 13:42:03 +1100

    modified AbnValidator to convert string abn to int array properly

--- src/Application/Common/Validators/AbnValidator.cs
+++ src/Application/Common/Validators/AbnValidator.cs
@@ -12,7 +12,7 @@ public class AbnValidator : AbstractValidator<string?>
             .Must(e =>
             {
                 int[] weights = [10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
-                int[] abn = Array.ConvertAll(e.Split(""), d => int.Parse(d));
+                int[] abn = Array.ConvertAll(e.ToCharArray(), digit => (int) char.GetNumericValue(digit));
                 abn[0]--;
                 int sum = 0;
                 for (int i = 0; i <= 11; i++)
