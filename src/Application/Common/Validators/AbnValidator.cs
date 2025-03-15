6a506e68b8b37f59b0bb48074049626ec0ae7c7b
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-16 10:20:13 +1100

    Refactored AbnValidator, added IsValidAbn method to run in validate call

--- src/Application/Common/Validators/AbnValidator.cs
+++ src/Application/Common/Validators/AbnValidator.cs
@@ -11,24 +11,24 @@ public class AbnValidator : AbstractValidator<string?>
             .Cascade(CascadeMode.Stop)
             .Matches("^(\\d *?){11}$")
             .WithMessage("ABN must be an 11 digit number")
-            .Must(e =>
-            {
-                int[] weights = [10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
-                int[] abn = Array.ConvertAll(e.ToCharArray(), digit => (int) char.GetNumericValue(digit));
-                abn[0]--;
-                int sum = 0;
-                for (int i = 0; i < 11; i++)
-                {
-                    abn[i] *= weights[i];
-                    sum += abn[i];
-                }
-
-                return sum % 89 == 0;
-            })
+            .Must(IsValidAbn)
             .WithMessage("ABN is invalid")
             .When(e => !string.IsNullOrWhiteSpace(e));
     }
 
     #endregion
 
+    #region Methods
+
+    private bool IsValidAbn(string abn)
+    {
+        int[] weights = [10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+        int[] abnDigits = abn.Select(d => (int) char.GetNumericValue(d)).ToArray();
+        abnDigits[0]--;
+        int sum = abnDigits.Zip(weights, (digit, weight) => digit * weight).Sum();
+
+        return sum % 89 == 0;
+    }
+
+    #endregion
 }
