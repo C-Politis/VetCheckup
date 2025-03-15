24680ebe419e4d4691c136cb04de67827dea2e35
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-15 15:36:25 +1100

    fixed bugs in validation logic for AbnValidator. Validator now stops on first broken rule

--- src/Application/Common/Validators/AbnValidator.cs
+++ src/Application/Common/Validators/AbnValidator.cs
@@ -8,14 +8,16 @@ public class AbnValidator : AbstractValidator<string?>
     public AbnValidator()
     {
         _ = this.RuleFor(e => e)
+            .Cascade(CascadeMode.Stop)
             .Matches("^(\\d *?){11}$")
+            .WithMessage("ABN must be an 11 digit number")
             .Must(e =>
             {
                 int[] weights = [10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
                 int[] abn = Array.ConvertAll(e.ToCharArray(), digit => (int) char.GetNumericValue(digit));
                 abn[0]--;
                 int sum = 0;
-                for (int i = 0; i <= 11; i++)
+                for (int i = 0; i < 11; i++)
                 {
                     abn[i] *= weights[i];
                     sum += abn[i];
@@ -23,6 +25,7 @@ public class AbnValidator : AbstractValidator<string?>
 
                 return sum % 89 == 0;
             })
+            .WithMessage("ABN is invalid")
             .When(e => !string.IsNullOrWhiteSpace(e));
     }
 
