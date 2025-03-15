07a1248001671f3dea487dce56a7c61343200a3c
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-15 12:34:52 +1100

    AbnValidator written

---
+++ src/Application/Common/Validators/AbnValidator.cs
@@ -0,0 +1,25 @@
+﻿namespace VetCheckup.Application.Common.Validators;
+
+public class AbnValidator : AbstractValidator<string>
+{
+    public AbnValidator()
+    {
+        _ = this.RuleFor(e => e)
+            .Matches("^(\\d *?){11}$")
+            .Must(e =>
+            {
+                int[] weights = [10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+                int[] abn = Array.ConvertAll(e.Split(""), d => int.Parse(d));
+                abn[0]--;
+                int sum = 0;
+                for (int i = 0; i <= 11; i++)
+                {
+                    abn[i] *= weights[i];
+                    sum += abn[i];
+                }
+
+                return sum % 89 == 0;
+            })
+            .When(e => !string.IsNullOrWhiteSpace(e));
+    }
+}
