20a93f6dd0a2f3014b67b7567ddbddc89e0e6818
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-16 13:22:17 +1100

    UdpateOrganisationRequest written

---
+++ src/Application/UseCases/Organisations/UpdateOrganisation/UpdateOrganisationRequest.cs
@@ -0,0 +1,25 @@
+﻿using VetCheckup.Domain.Entities;
+using VetCheckup.Domain.Enums;
+
+namespace VetCheckup.Application.UseCases.Organisations.UpdateOrganisation;
+
+public class UpdateOrganisationRequest : IRequest
+{
+
+    #region Properties
+
+    public required Guid OrganisationId { get; set; }
+
+    public string? Abn { get; set; }
+
+    public Address? Address { get; set; }
+
+    public Contact? ContactDetails { get; set; }
+
+    public string? Name { get; set; }
+
+    public OrganisationType? OrganisationType { get; set; }
+
+    #endregion
+
+}
