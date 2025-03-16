37750824d7abaf40ae7c84f5451a3ba97ade4b08
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-16 13:50:20 +1100

    UpdateOrganisationInteractor written

---
+++ src/Application/UseCases/Organisations/UpdateOrganisation/UpdateOrganisationInteractor.cs
@@ -0,0 +1,21 @@
+﻿using VetCheckup.Application.Services.Persistence;
+using VetCheckup.Domain.Entities;
+
+namespace VetCheckup.Application.UseCases.Organisations.UpdateOrganisation;
+
+public class UpdateOrganisationInteractor(IDbContext context, IMapper mapper) : IRequestHandler<UpdateOrganisationRequest>
+{
+
+    #region Methods
+
+    Task IRequestHandler<UpdateOrganisationRequest>.Handle(UpdateOrganisationRequest request, CancellationToken cancellationToken)
+    {
+        var organisation = context.Get<Organisation>().FirstOrDefault(org => org.OrganisationId == request.OrganisationId) ?? throw new Exception("Organisation not found.");
+        mapper.Map(request, organisation);
+
+        return Task.CompletedTask;
+    }
+
+    #endregion
+
+}
