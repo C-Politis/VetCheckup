726d268792a82ccc2bd73865cf5876cac62fc23b
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-16 11:09:11 +1100

    OrganisationDto written

---
+++ src/Application/Dtos/OrganisationDto.cs
@@ -0,0 +1,26 @@
+﻿using VetCheckup.Domain.Entities;
+using VetCheckup.Domain.Enums;
+
+namespace VetCheckup.Application.Dtos
+{
+    public class OrganisationDto
+    {
+
+        #region Properties
+
+        public string? Abn { get; set; }
+
+        public required Address Address { get; set; }
+
+        public required Contact ContactDetails { get; set; }
+
+        public required string Name { get; set; }
+
+        public Guid OrganisationId { get; set; }
+
+        public required OrganisationType OrganisationType { get; set; }
+
+        #endregion
+
+    }
+}
