8bd241e62cea6d5b944a6cb79293a727b1c9007a
Christian Politis <christianpolitis@gmail.com>
2025-03-16 15:28:13 +1100

    Update UpdatePetProfile.cs

--- src/Application/UseCases/Pets/UpdatePet/UpdatePetProfile.cs
+++ src/Application/UseCases/Pets/UpdatePet/UpdatePetProfile.cs
@@ -5,11 +5,33 @@ namespace VetCheckup.Application.UseCases.Pets.UpdatePet
 
     public class UpdatePetProfile : Profile
     {
+        
         #region Constructors
 
         public UpdatePetProfile()
             => CreateMap<UpdatePetRequest, Pet>()
-                        .ForMember(destination => destination.Owner, source => source.Ignore());
+                .ForMember(dest => dest.Name, opts =>
+                {
+                    opts.PreCondition(src => src.Name != null);
+                    opts.MapFrom(src => src.Name);
+                })
+                .ForMember(dest => dest.Species, opts =>
+                {
+                    opts.PreCondition(src => src.Species != null);
+                    opts.MapFrom(src => src.Species);
+                })
+                .ForMember(dest => dest.Sex, opts =>
+                {
+                    opts.PreCondition(src => src.Sex != null);
+                    opts.MapFrom(src => src.Sex);
+                })
+                .ForMember(dest => dest.MicrochipId, opts =>
+                {
+                    opts.PreCondition(src => src.MicrochipId != null);
+                    opts.MapFrom(src => src.MicrochipId);
+                })
+                .ForMember(dest => dest.Owner, opts => opts.Ignore())
+                .ForMember(dest => dest.PetId, opts => opts.Ignore());
 
         #endregion
 
