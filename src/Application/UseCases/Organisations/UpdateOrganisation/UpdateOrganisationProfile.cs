8955f3c223b210c6f5ed3418608d97286eac07b6
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-16 14:00:12 +1100

    UpdateOrganisationProfile written

---
+++ src/Application/UseCases/Organisations/UpdateOrganisation/UpdateOrganisationProfile.cs
@@ -0,0 +1,15 @@
+﻿using VetCheckup.Domain.Entities;
+
+namespace VetCheckup.Application.UseCases.Organisations.UpdateOrganisation;
+
+public class UpdateOrganisationProfile : Profile
+{
+
+    #region Constructors
+
+    public UpdateOrganisationProfile()
+        => CreateMap<UpdateOrganisationRequest, Organisation>();
+
+    #endregion
+
+}
