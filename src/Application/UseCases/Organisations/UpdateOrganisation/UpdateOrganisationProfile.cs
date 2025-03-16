075f66870ee08d8e90da0262e8a5a6cf6c6fcd92
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-16 15:09:29 +1100

    fixed UpdateOrganisationProfile to only map nullable fields when not null

--- src/Application/UseCases/Organisations/UpdateOrganisation/UpdateOrganisationProfile.cs
+++ src/Application/UseCases/Organisations/UpdateOrganisation/UpdateOrganisationProfile.cs
@@ -8,8 +8,26 @@ public class UpdateOrganisationProfile : Profile
     #region Constructors
 
     public UpdateOrganisationProfile()
-        => CreateMap<UpdateOrganisationRequest, Organisation>();
-
+    {
+        _ = this.CreateMap<UpdateOrganisationRequest, Organisation>()
+            .ForMember(dest => dest.Abn, opts =>
+            {
+                opts.PreCondition(src => src.Abn != null);
+                opts.MapFrom(src => src.Abn);
+            })
+            .ForMember(dest => dest.Name, opts =>
+            {
+                opts.PreCondition(src => src.Name != null);
+                opts.MapFrom(src => src.Name);
+            })
+            .ForMember(dest => dest.OrganisationType, opts =>
+            {
+                opts.PreCondition(src => src.OrganisationType != null);
+                opts.MapFrom(src => src.OrganisationType);
+            })
+            .ForMember(dest => dest.OrganisationId, opts => opts.Ignore());
+    }
+            
     #endregion
 
 }
