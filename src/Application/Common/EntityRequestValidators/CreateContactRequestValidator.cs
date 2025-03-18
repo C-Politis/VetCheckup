3b8b1c39ef886749c4caede123f4d12da1674444
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-18 21:48:07 +1100

    create and update contact request validators take mobile as string, and expect max size of 15 digits

--- src/Application/Common/EntityRequestValidators/CreateContactRequestValidator.cs
+++ src/Application/Common/EntityRequestValidators/CreateContactRequestValidator.cs
@@ -14,7 +14,8 @@ public class CreateContactRequestValidator : AbstractValidator<CreateContactRequ
             .When(e => !string.IsNullOrWhiteSpace(e.Email));
 
         _ = this.RuleFor(e => e.Mobile)
-            .GreaterThan(0);
+            .MaximumLength(15)
+            .When(e => !string.IsNullOrWhiteSpace(e.Mobile));
     }
 
     #endregion
