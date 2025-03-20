990df36bbfc8e768fd15f6f1b881023c670ad85b
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-20 13:54:37 +1100

    Updated configs to reflect changes to Entities

--- src/Infrastructure/Configurations/AddressConfiguration.cs
+++ src/Infrastructure/Configurations/AddressConfiguration.cs
@@ -17,18 +17,23 @@ public class AddressConfiguration : IEntityTypeConfiguration<Address>
             .ValueGeneratedOnAdd();
 
         builder.Property(e => e.Country)
+            .IsRequired()
             .HasMaxLength(100);
 
         builder.Property(e => e.PostalCode)
+            .IsRequired()
             .HasMaxLength(20);
 
         builder.Property(e => e.State)
+            .IsRequired()
             .HasMaxLength(100);
 
         builder.Property(e => e.StreetAddress)
+            .IsRequired()
             .HasMaxLength(250);
 
         builder.Property(e => e.Suburb)
+            .IsRequired()
             .HasMaxLength(100);
 
         builder.HasKey(e => e.AddressId);
