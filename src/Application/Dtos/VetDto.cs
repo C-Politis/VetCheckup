7d05df1b8dc097e72e2bc4b042ce66e67f14fcc9
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-21 22:29:42 +1100

    VetDto written

---
+++ src/Application/Dtos/VetDto.cs
@@ -0,0 +1,23 @@
+﻿using VetCheckup.Domain.Entities;
+
+namespace VetCheckup.Application.Dtos
+{
+    public class VetDto
+    {
+
+        #region Properties
+
+        public required Address Address { get; set; }
+
+        public required Contact ContactDetails { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public required string Name { get; set; }
+
+        public Guid VetId { get; set; }
+
+        #endregion
+
+    }
+}
