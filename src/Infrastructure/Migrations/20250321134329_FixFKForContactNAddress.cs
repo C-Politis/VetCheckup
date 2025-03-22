9cd6cdd100deea9c9e351c979fdfe06597375ba7
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-22 21:03:40 +1100

    Added mapping for Vet and VetDto

---
+++ src/Infrastructure/Migrations/20250321134329_FixFKForContactNAddress.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VetCheckup.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixFKForContactNAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AddressId",
+                table: "Vet",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ContactId",
+                table: "Vet",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AddressId",
+                table: "Owner",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ContactId",
+                table: "Owner",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "AddressId",
+                table: "Organisation",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ContactId",
+                table: "Organisation",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Vet");
+
+            migrationBuilder.DropColumn(
+                name: "ContactId",
+                table: "Vet");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Owner");
+
+            migrationBuilder.DropColumn(
+                name: "ContactId",
+                table: "Owner");
+
+            migrationBuilder.DropColumn(
+                name: "AddressId",
+                table: "Organisation");
+
+            migrationBuilder.DropColumn(
+                name: "ContactId",
+                table: "Organisation");
+        }
+    }
+}
