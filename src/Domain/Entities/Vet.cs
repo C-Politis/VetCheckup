5fa718ba79e3b921e8d3bf9692af10bb8565968a
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-03-24 16:41:23 +1100

    updated name fields in Vet Domain Entity

--- src/Domain/Entities/Vet.cs
+++ src/Domain/Entities/Vet.cs
@@ -1,4 +1,6 @@
-﻿namespace VetCheckup.Domain.Entities;
+﻿using VetCheckup.Application.Common.Enums;
+
+namespace VetCheckup.Domain.Entities;
 
 public class Vet
 {
@@ -11,7 +13,15 @@ public class Vet
 
     public required DateTime DateOfBirth { get; set; }
 
-    public required string Name { get; set; }
+    public required Title Title { get; set; }
+
+    public required string FirstName { get; set; }
+
+    public required string MiddleName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public required Suffix Suffix { get; set; }
 
     public required Guid VetId { get; set; }
 
