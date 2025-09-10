e26d8e3e1d4bda1f516987dbb5e6b8b7cb9b8d96
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-09-10 20:33:13 +1000

    User Configuration Written

---
+++ src/Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VetCheckup.Domain.Enums;
+
+namespace VetCheckup.Infrastructure.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+
+    #region Methods
+
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable(nameof(User));
+
+        builder.Property(e => e.Password)
+            .IsRequired();
+
+        builder.Property(e => e.UserName)
+            .IsRequired()
+            .HasMaxLength(20);
+
+        builder.Property(e => e.UserId)
+            .IsRequired()
+            .ValueGeneratedOnAdd()
+            .HasDefaultValueSql("NEWID()");
+
+        builder.Property(e => e.UserType)
+            .HasConversion(propVal => (int)propVal, dbVal => (UserType)dbVal)
+            .HasColumnType("int")
+            .IsRequired();
+
+        builder.HasKey(e => e.UserId);
+    }
+
+    #endregion
+
+}
