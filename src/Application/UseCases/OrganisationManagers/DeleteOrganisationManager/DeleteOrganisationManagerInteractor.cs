ba12a69547d8d43494f3f46950234c5c4119c740
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-09-12 06:41:57 +1000

    AB#18 DeleteOrganisationManager Use case files created

---
+++ src/Application/UseCases/OrganisationManagers/DeleteOrganisationManager/DeleteOrganisationManagerInteractor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetCheckup.Application.UseCases.OrganisationManagers.DeleteOrganisationManager
+{
+    class DeleteOrganisationManagerInteractor
+    {
+    }
+}
