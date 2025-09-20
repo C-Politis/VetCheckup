da03b69d74616f94c199e8271636125d8810a5e8
Ahmed El-Sayed <z5309219@ad.unsw.edu.au>
2025-09-20 16:36:14 +1000

    AB#20 DeleteOwner Usecase and tests complete and passing

---
+++ src/Application/UseCases/Owners/DeleteOwner/DeleteOwnerInteractor.cs
@@ -0,0 +1,19 @@
+﻿using VetCheckup.Application.Services.Persistence;
+using VetCheckup.Domain.Entities;
+
+namespace VetCheckup.Application.UseCases.Owners.DeleteOwner;
+
+public class DeleteOwnerInteractor(IDbContext context) : IRequestHandler<DeleteOwnerRequest>
+{
+
+    #region Methods
+
+    Task IRequestHandler<DeleteOwnerRequest>.Handle(DeleteOwnerRequest request, CancellationToken cancellationToken)
+    {
+        var owner = context.Get<Owner>().FirstOrDefault(e => e.OwnerId == request.OwnerId) ?? throw new Exception("Owner not found");
+        context.Remove(owner);
+        return Task.CompletedTask;
+    }
+
+    #endregion
+}
---
+++ tests/Application.UnitTests/UseCases/Owners/DeleteOwner/DeleteOwnerInteractorTests.cs
@@ -0,0 +1,100 @@
+﻿using MediatR;
+using Moq;
+using VetCheckup.Application.Services.Persistence;
+using VetCheckup.Application.UseCases.Owners.DeleteOwner;
+using VetCheckup.Domain.Entities;
+using VetCheckup.Domain.Enums;
+using Xunit;
+
+namespace VetCheckup.Application.UnitTests.UseCases.Owners.DeleteOwner;
+
+public class DeleteOwnerInteractorTests
+{
+
+    #region Mithril
+
+    private readonly Mock<IDbContext> _mockDbContext = new();
+    private readonly DeleteOwnerRequest _request = new()
+    {
+        OwnerId = Guid.NewGuid()
+    };
+    private readonly IRequestHandler<DeleteOwnerRequest> _interactor;
+
+    #endregion
+
+    #region Constructor
+
+    public DeleteOwnerInteractorTests()
+    {
+        this._mockDbContext
+            .Setup(mock => mock.Get<Owner>())
+            .Returns(new[] { new Owner()
+                {
+                    OwnerId = _request.OwnerId,
+                    Address = new()
+                    {
+                        AddressId = Guid.NewGuid(),
+                        Country = "Test Country",
+                        PostalCode = "Test Postal Code",
+                        State = "Test State",
+                        StreetAddress = "Test Street Address",
+                        Suburb = "Test Suburb",
+                    },
+                    ContactDetails = new()
+                    {
+                        ContactId = Guid.NewGuid(),
+                        Email = string.Empty,
+                        Mobile = string.Empty
+                    },
+                    User = new()
+                    {
+                        UserId =Guid.NewGuid(),
+                        UserName = "MyUser",
+                        Password = "Password",
+                        UserType = UserType.OrganisationManager
+                    },
+                    FirstName = "Test",
+                    LastName = "Owner",
+                    MiddleName = "Middle",
+                    Suffix = Suffix.Esq,
+                    Title = Title.Dr,
+                    Pets = new List<Pet>(),
+                    DateOfBirth = DateTime.MinValue
+                }
+            }.AsQueryable());
+
+        this._interactor = new DeleteOwnerInteractor(this._mockDbContext.Object);
+    }
+
+    #endregion
+
+    #region Tests
+
+    [Fact]
+    public async Task DeleteOwner_WhenOwnerExists()
+    {
+        // Act
+        await _interactor.Handle(_request, CancellationToken.None);
+
+        // Assert
+        this._mockDbContext.Verify(mock => mock.Remove(It.IsAny<Owner>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteOwner_ThrowsExceptionWhenOwnerNotFound()
+    {
+        // Arrange
+        var invalidRequest = new DeleteOwnerRequest
+        {
+            OwnerId = Guid.NewGuid()
+        };
+
+        // Act
+        await Assert.ThrowsAsync<Exception>(() => _interactor.Handle(invalidRequest, CancellationToken.None));
+
+        // Assert
+        this._mockDbContext.Verify(mock => mock.Remove(It.IsAny<Owner>()), Times.Never);
+    }
+
+    #endregion
+}
